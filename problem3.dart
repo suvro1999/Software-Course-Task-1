4d4f26d321df21bbde59de53706588c40fb46d3f
subroto suvro <47747335+suvro1999@users.noreply.github.com>
2021-10-05 00:42:54 +0600

    Create problem3.dart

---
+++ problem3.dart
@@ -0,0 +1,18 @@
+void main() {
+      
+    int num=5;
+
+    if(num > 0)
+    {
+        print("Number is POSITIVE");
+    }
+    if(num < 0)
+    {
+        print("Number is NEGATIVE");
+    }
+    if(num == 0)
+    {
+        print("Number is ZERO");
+    }
+
+}
