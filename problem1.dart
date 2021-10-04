05151c2df9ceb2b633e6fd5978c4029ac4e4abe0
subroto suvro <47747335+suvro1999@users.noreply.github.com>
2021-10-05 00:40:21 +0600

    Create problem1.dart

---
+++ problem1.dart
@@ -0,0 +1,17 @@
+void main() {
+
+  int num1=2,num2=6;
+   if(num1 > num2)
+    {
+        print("maximum $num1" );        
+    }
+    if(num2 > num1)
+    {
+        print("maximum $num2" );
+    }
+    if(num1 == num2)
+    {
+        print("Both are equal");
+  
+  }
+}
