750ed73b8968910350f0956f6e4e4b608b37185e
subroto suvro <47747335+suvro1999@users.noreply.github.com>
2021-10-05 00:43:28 +0600

    Create problem4.dart

---
+++ problem4.dart
@@ -0,0 +1,14 @@
+void main() {
+      
+    int num=5;
+
+  if((num % 5 == 0) && (num % 11 == 0))
+    {
+        print("Number is divisible by 5 and 11");
+    }
+    else
+    {
+        print("Number is not divisible by 5 and 11");
+    }
+}
+
